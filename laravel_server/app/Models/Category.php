3d428bad5fe12793f342244d9e707bea0bd05718
SoufianeAbk <soufiane.abakkiou@student.ehb.be>
2025-08-11 18:31:16 +0200

    Categorie_model

---
+++ laravel_server/app/Models/Category.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'image_url',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    public function jerseys()
+    {
+        return $this->hasMany(Jersey::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
